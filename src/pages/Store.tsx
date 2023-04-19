759d97425b783d1c4eb12a8be968083936270d59
Rohit Mondal <rray524@gmail.com>
2023-04-19 19:30:49 +0530

    store page and it's property from json file

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import StoreItem from "../components/StoreItem";
 
 const Store = () => {
   return (
-    <div>
+    <>
       <h2>Store</h2>
-    </div>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item, i) => (
+          <Col key={i}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
